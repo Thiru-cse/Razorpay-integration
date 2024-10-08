a276b62ea4e02220e56b314fdd0d999e5bad426a
Thiru-cse <thiru.c.cse@gmail.com>
2024-10-08 15:53:13 +0530

    Deployment link

--- Frontend/razorpay/src/api/paymentApi.jsx
+++ Frontend/razorpay/src/api/paymentApi.jsx
@@ -2,7 +2,7 @@ import axios from "axios";
 
 // Set up the base URL for API calls
 const instance = axios.create({
-  baseURL: "http://localhost:5000/api", // Make sure this is your backend's URL
+  baseURL: "https://razorpay-integration-m7jr.onrender.com/api", // Make sure this is your backend's URL
   headers: {
     "Content-Type": "application/json",
   },
